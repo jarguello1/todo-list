1bbe1c3bbdd1fb8e547952ccf3a6137c3c758f8d
jarguello1 <jcarguello11@live.com>
2022-09-06 15:52:07 -0400

    Add form to task container

--- src/tasks.js
+++ src/tasks.js
@@ -7,7 +7,34 @@ function createTaskBox() {
     title.textContent = "Tasks";
     taskContainer.appendChild(title);
 
+
+    taskContainer.appendChild(createForm());
+
     return taskContainer;
+}
+
+function createForm() {
+    const form = document.createElement('form');
+    form.classList.add('taskForm');
+    form.setAttribute('id', 'taskFormInput');
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    form.appendChild(input);
+
+    const addBtn = document.createElement('button');
+    addBtn.classList.add('add');
+    addBtn.textContent = "Add";
+    form.appendChild(addBtn);
+
+    const cancelBtn = document.createElement('button');
+    cancelBtn.classList.add('cancel');
+    cancelBtn.textContent = "Cancel"
+    form.appendChild(cancelBtn);
+
+    form.style.display = "none";
+
+    return form;
 }
 
 function loadTaskContainer() {
