bf38a10973d1e6350d3d013c07bc24c4397f546c
jarguello1 <jcarguello11@live.com>
2022-09-07 14:34:58 -0400

    Add projects object

--- src/projects.js
+++ src/projects.js
@@ -1,8 +1,54 @@
-function Project(name) {
-    this.name = name;
-    let tasklist = [];
-}
+import { toDate, isToday, isThisWeek, subDays } from 'date-fns'
 
-let allTasks = new Project('All Tasks');
-let todayTasks = new Project('Today');
-let weekTasks = new Project('Today');
+export default function Project(name) {
+    this.name = name
+    this.tasks = []
+  
+
+  this.setName = function(name) {
+    this.name = name
+  }
+
+  this.getName = function() {
+    return this.name
+  }
+
+  this.setTasks = function(tasks) {
+    this.tasks = tasks
+  }
+
+  this.getTasks = function() {
+    return this.tasks
+  }
+
+  this.getTask = function(taskName) {
+    return this.tasks.find((task) => task.getName() === taskName)
+  }
+
+  this.contains = function(taskName) {
+    return this.tasks.some((task) => task.getName() === taskName)
+  }
+
+  this.addTask = function(newTask) {
+    if (this.tasks.find((task) => task.getName() === newTask.name)) return
+    this.tasks.push(newTask)
+  }
+
+  this.deleteTask = function(taskName) {
+    this.tasks = this.tasks.filter((task) => task.name !== taskName)
+  }
+
+  this.getTasksToday = function() {
+    return this.tasks.filter((task) => {
+      const taskDate = new Date(task.getDateFormatted())
+      return isToday(toDate(taskDate))
+    })
+  }
+
+  this.getTasksThisWeek = function() {
+    return this.tasks.filter((task) => {
+      const taskDate = new Date(task.getDateFormatted())
+      return isThisWeek(subDays(toDate(taskDate), 1))
+    })
+  }
+}
