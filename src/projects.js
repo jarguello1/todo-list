bac348ca4f7222f0d081dab4a2ffb61c5c64c521
jarguello1 <jcarguello11@live.com>
2022-09-07 14:24:35 -0400

    Create Task object

--- src/projects.js
+++ src/projects.js
@@ -3,11 +3,6 @@ function Project(name) {
     let tasklist = [];
 }
 
-function Task(name, date = "No date") {
-    this.name = name;
-    this.date = date;
-}
-
 let allTasks = new Project('All Tasks');
 let todayTasks = new Project('Today');
 let weekTasks = new Project('Today');
