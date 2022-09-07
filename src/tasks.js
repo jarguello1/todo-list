3164c3fe8aba325895925062f9f58602b4f7807e
jarguello1 <jcarguello11@live.com>
2022-09-07 17:59:34 -0400

    add project item to tasks, add semicolons

--- src/tasks.js
+++ src/tasks.js
@@ -1,28 +1,29 @@
 
-export default function Task(name, dueDate = 'No date') {
-      this.name = name
-      this.dueDate = dueDate
+export default function Task(name, dueDate = 'no date', project = 'no project') {
+      this.name = name;
+      this.dueDate = dueDate;
+      this.project = project;
   
     this.setName = function(name) {
-      this.name = name
+      this.name = name;
     }
   
     this.getName = function() {
-      return this.name
+      return this.name;
     }
   
     this.setDate = function(dueDate) {
-      this.dueDate = dueDate
+      this.dueDate = dueDate;
     }
   
     this.getDate = function() {
-      return this.dueDate
+      return this.dueDate;
     }
   
     this.getDateFormatted = function() {
-      const day = this.dueDate.split('/')[0]
-      const month = this.dueDate.split('/')[1]
-      const year = this.dueDate.split('/')[2]
-      return `${month}/${day}/${year}`
+      const day = this.dueDate.split('/')[0];
+      const month = this.dueDate.split('/')[1];
+      const year = this.dueDate.split('/')[2];
+      return `${month}/${day}/${year}`;
     }
   }
